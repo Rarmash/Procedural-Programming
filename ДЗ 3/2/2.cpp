46d47c385e44a8778a5ffbba0e173f008744d34d
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-09-14 10:26:12 +0300

    Update 2.cpp

--- ДЗ 3/2/2.cpp
+++ ДЗ 3/2/2.cpp
@@ -5,12 +5,19 @@ using namespace std;
 int main()
 {
     setlocale(0, "");
-    double S, m, n, p;
+    double S, m, n, p, r;
     cout << "Введите величину ссуды: ";
     cin >> S;
     cout << "Введите месячную выплату: ";
     cin >> m;
     cout << "Введите количество лет: ";
     cin >> n;
-    double d1 = pow()
+    for (p = 1; p <= 100; p++) {
+        r = p / 100;
+        double mm = (S * r * pow((1 + r), n)) / (12 * (pow((1 + r), n) - 1));
+        if (mm > m) {
+            cout << "Процент равен " << p << ".";
+            exit(0);
+        }
+    }
 }
