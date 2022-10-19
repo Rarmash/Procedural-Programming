be46eebbc9d5073c940a8e0244ebc4ae2ce9a19a
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-10-19 19:18:05 +0300

    Update 2.cpp

--- ДЗ 4/2/2.cpp
+++ ДЗ 4/2/2.cpp
@@ -1,32 +1,30 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 template <class T>
-string sign(T& a)
+void sign(T& a)
 {
-    string f;
     if (a > 0)
     {
-        f = "+";
+        a = 1;
     }
     if (a == 0)
     {
-        f = "Нейтрально";
+        a = 0;
     }
     if (a < 0)
     {
-        f = "-";
+        a = -1;
     }
-    return f;
 }
 
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    cout << "Введите число для определения его знака." << endl;
+    cout << "Введите число для определения его знака. (0 - \"0\", 1 - \"+\", -1 - \"-\")" << endl;
     double q;
     cin >> q;
-    cout << sign(q) << endl;
+    sign(q);
+    cout << q << endl;
 }
