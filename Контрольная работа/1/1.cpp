d16e6af910e47e0267971926d1b40713fd5f7b98
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-11-16 13:47:50 +0300

    Hotfix

--- Контрольная работа/1/1.cpp
+++ Контрольная работа/1/1.cpp
@@ -6,7 +6,12 @@ int main()
     setlocale(0, "");
     int a, b, n, c;
     cin >> a >> b >> c;
-    int s1 = c - a;
-    int s2 = s1 / b;
-    cout << s2;
+    if (a <= c && a > 0 && b > 0 && c > 0) {
+        int s1 = c - a;
+        int s2 = s1 / b;
+        cout << s2;
+    }
+    else {
+        cout << "Error.";
+    }
 }
