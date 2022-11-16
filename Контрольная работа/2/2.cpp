092bd60c0e64af5e3b8dd1954732eaca6b01c2d4
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-11-16 12:55:16 +0300

    Снова спиннеры

---
+++ Контрольная работа/2/2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k;
+    cin >> k;
+    if (k < 3 || k == 5) {
+        cout << "0\n0";
+    }
+    else if (k % 3 == 0) {
+        cout << k / 3 << endl << 0;
+    }
+    else {
+        cout << k % 4 << endl << (k - 3 * (k % 4)) / 4;
+    }
+}
