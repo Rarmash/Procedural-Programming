2012032bc27a8082f2091f333e437b770abd33c5
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-11-16 12:42:16 +0300

    Спиннеры

---
+++ Контрольная работа/1/1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    int a, b, n, c;
+    cin >> a >> b >> c;
+    int s1 = c - a;
+    int s2 = s1 / b;
+    cout << s2;
+}
