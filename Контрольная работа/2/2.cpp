6429c53e84d4ee967371733eb4c4627606f145c2
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-11-16 13:52:19 +0300

    Hotfix

--- Контрольная работа/2/2.cpp
+++ Контрольная работа/2/2.cpp
@@ -3,15 +3,20 @@ using namespace std;
 
 int main()
 {
-    int k;
-    cin >> k;
-    if (k < 3 || k == 5) {
-        cout << "0\n0";
-    }
-    else if (k % 3 == 0) {
-        cout << k / 3 << endl << 0;
+    int m;
+    cin >> m;
+    if (m > 0) {
+        if (m < 3 || m == 5) {
+            cout << "0\n0";
+        }
+        else if (m % 3 == 0) {
+            cout << m / 3 << endl << 0;
+        }
+        else {
+            cout << m % 4 << endl << (m - 3 * (m % 4)) / 4;
+        }
     }
     else {
-        cout << k % 4 << endl << (k - 3 * (k % 4)) / 4;
+        cout << "Error.";
     }
 }
