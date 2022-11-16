0e810d292eddeb339655d74a1bc3ed6bfe0064ec
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-11-16 18:08:51 +0300

    Update ControlWork1.cpp

--- ControlWork1/ControlWork1.cpp
+++ ControlWork1/ControlWork1.cpp
@@ -10,9 +10,8 @@ void task1()
     cout << "Введите стоимость одной лопасти: "; cin >> b;
     cout << "Введите максимальную стоимость всего спиннера: "; cin >> c;
     if (a <= c && a > 0 && b > 0 && c > 0) {
-        int s1 = c - a;
-        int s2 = s1 / b;
-        cout << "Максимальное число лопастей спиннера - " << s2 << ".";
+        n = c - a / b; //Формула, обратная a+b*n=c
+        cout << "Максимальное число лопастей спиннера - " << n << ".";
     }
     else if (a > c) {
         cout << "Ошибка! Стоимость основания спиннера не может быть больше допустимой стоимости всего спиннера!";
@@ -28,29 +27,27 @@ void task2()
 {
     int m, a = 0, b = 0;
     cout << "Введите количество лопастей, которое есть у Дениса: "; cin >> m;
-    int m1 = m;
+    int m_orig = m;
     while (m >= 0) {
         if (m % 3 == 0)
         {
-            a = a + (m / 3);
+            a += (m / 3); //если достаточно лопастей для создания спиннеров по 3 лопасти
             break;
         }
         if (m % 4 == 0)
         {
-            b = b + (m / 4);
+            b += (m / 4); //то же самое, что и выше, но с 4 лопастями. НО СРЕДИ ОСТАВШИХСЯ С ПРОШЛОЙ ОПЕРАЦИИ
             break;
         }
-        if (m % 3 == 1 || m % 3 == 2)
+        if (m % 3 == 2 || m % 3 == 1)
         {
-            m = m - 4; b++;
+            m -= 4; b++; //попытка изменения количества для получения спиннера с 4 лопастями
         }
     }
-    if ((3 * a + 4 * b) != m1)
-    {
-        cout << "0 спиннеров с 3 лопастями\n0 спиннеров с 4 лопастями\nНевозможно произвести спиннеры так, чтобы суммарное число лопастей было равно " << m1;
+    if ((3 * a + 4 * b) != m_orig) {
+        cout << "0 спиннеров с 3 лопастями\n0 спиннеров с 4 лопастями\nНевозможно произвести спиннеры так, чтобы суммарное число лопастей было равно " << m_orig;
     }
-    else
-    {
+    else {
         cout << a << " спиннеров с 3 лопастями" << endl << b << " спиннеров с 4 лопастями";
     }
     cout << endl;
@@ -71,58 +68,37 @@ void task3()
     system("pause");
 }
 
-void task4()
+void task4() //UNSTABLE
 {
-    /*
-    const unsigned int PLACES_COUNT = 54;
-
-    vector<unsigned int> statuses; // Сколько свободно мест (от 0 до 6, 6 - купе свободно)
-    map<unsigned int, unsigned int> links; // Словарь Место - Номер купе.
-
-
-    void init() {
-        for (unsigned int i = 0; i < PLACES_COUNT / 6; i++) {
-            //std::cout << "Generating for " << i << std::endl;
-            statuses.push_back(0);
-            for (unsigned int j : {4 * i + 1, 4 * i + 2, 4 * i + 3, 4 * i + 4, PLACES_COUNT - i * 2 - 1, PLACES_COUNT - i * 2}) {
-                //std::cout << " * Added : " << j << std::endl;
-                links[j] = i;
-            }
-        }
-
-
+    setlocale(0, "");
+    cout << "Введите кол-во свободных мест: ";
+    int n, pl;
+    int k[9]{};
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> pl;
+        if (((pl >= 1) and (pl <= 4)) or (pl == 53) or (pl == 54)) k[0]++;
+        else if (((pl >= 5) and (pl <= 8)) or (pl == 51) or (pl == 52)) k[1]++;
+        else if (((pl >= 9) and (pl <= 12)) or (pl == 49) or (pl == 50)) k[2]++;
+        else if (((pl >= 13) and (pl <= 16)) or (pl == 47) or (pl == 48)) k[3]++;
+        else if (((pl >= 17) and (pl <= 20)) or (pl == 45) or (pl == 46)) k[4]++;
+        else if (((pl >= 21) and (pl <= 24)) or (pl == 43) or (pl == 44)) k[5]++;
+        else if (((pl >= 25) and (pl <= 28)) or (pl == 41) or (pl == 42)) k[6]++;
+        else if (((pl >= 29) and (pl <= 32)) or (pl == 39) or (pl == 40)) k[7]++;
+        else if (((pl >= 33) and (pl <= 36)) or (pl == 37) or (pl == 38)) k[8]++;
     }
-
-
-    int main() {
-        init();
-        unsigned int buffer, n;
-        std::cin >> n;
-        for (int i = 0; i < n; i++) {
-            //std::cout << ">>";
-            std::cin >> buffer;
-            if (links.find(buffer) != links.end()) { // Проверка валидности ключа
-                statuses[links[buffer]] += 1;
-                links.erase(buffer);
-            }
+    int l = 0, max = 0;
+    for (int i = 0; i <= 8; i++)
+    {
+        if ((k[i] == 6) and (k[i + 1] == 6)) l++;
+        else
+        {
+            if (l > max) max = l;
+            l = 0;
         }
-
-        unsigned int result = 0;
-        unsigned int now = 0;
-        for (unsigned int i : statuses) {
-            if (i == 6) {
-                now += 1;
-                if (now > result)
-                    result = now;
-            }
-            else {
-                now = 0;
-            }
-        }
-        cout << result << std::endl;
     }
-    */
-    cout << "4.";
+    cout << max + 1;
 }
 
 void task5() {
