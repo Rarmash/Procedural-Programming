b81da6e2b07514753e9be0423f8a3f46a040f3ad
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-11-23 18:22:41 +0300

    5.3 (6) + pifagor

---
+++ ДЗ 5/3 (6)/3 (6).cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string str;
+    ifstream fin;
+    fin.open("lorem.txt");
+    getline(fin, str);
+    istringstream iss(str);
+    cout << "Слово минимальной длины: " << *min_element(istream_iterator<string>(iss),
+        istream_iterator<string>(),
+        [](const string& s1, const string& s2)
+        {return s1.length() < s2.length(); });
+    cout << endl;
+}
