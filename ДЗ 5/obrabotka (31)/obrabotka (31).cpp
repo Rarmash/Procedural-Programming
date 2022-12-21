52ca73043ec965303ee8893068f6953163e7bb29
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-12-21 16:49:10 +0300

    Update obrabotka (31).cpp

--- ДЗ 5/obrabotka (31)/obrabotka (31).cpp
+++ ДЗ 5/obrabotka (31)/obrabotka (31).cpp
@@ -18,24 +18,33 @@ int main()
         cout << "Выберите вариант поиска: " << endl;
         cout << "1. Найти максимальное по длине слово" << endl;
         cout << "2. Найти минимальное по длине слово" << endl;
-        cin >> n;
+        cout << "Номер: "; cin >> n;
+        string res{ "" }, tmp{ "" };
         switch(n) {
-        case 1:
-            string res{ "" }, tmp{ "" };
-            for (int i{ 0 }; i < len; ++i) {
-                if (str[i] != ' ' ) tmp += str[i];
-                if (str[i] == ' ' || i == len - 1) {
-                    if (res.length() < tmp.length()) res = tmp;
-                    tmp = "";
+            case 1:
+                res = ""; tmp = "";
+                for (int i{ 0 }; i < len; ++i) {
+                    if (str[i] != ' ' || str[i] != ',') tmp += str[i];
+                    if (str[i] == ' ' || str[i] == ',' || i == len - 1) {
+                        if (res.length() < tmp.length()) res = tmp;
+                        tmp = "";
+                    }
                 }
-            }
-            cout << res;
-            exit(0);
-        /*case 2:
-            pass;
-        default:
-            return true;
-            */
+                cout << res;
+                exit(0);
+            case 2:
+                res = str; tmp = "";
+                for (int i{ 0 }; i < len; ++i) {
+                    if (str[i] != ' ' || str[i] != ',') tmp += str[i];
+                    if ((str[i] == ' ' || str[i] == ',' || i == len - 1) && tmp != "" && tmp != " ") {
+                        if (res.length() > tmp.length()) res = tmp;
+                        tmp = "";
+                    }
+                }
+                cout << res;
+                exit(0);
+            default:
+                cout << "Такого номера нет!" << endl; continue;
         }
     }
 }
